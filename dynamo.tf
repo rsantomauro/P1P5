d2195cdc2babb55e068343d5fe391b25c755f114
Rodrigo Santomauro <rodrigoolema@outlook.com>
2022-04-27 20:17:07 -0300

    Final Practico 5 funcionando

--- dynamo.tf
+++ dynamo.tf
@@ -1,14 +1,14 @@
-resource "aws_dynamodb_table" "dynamodb_terraform_cloud_lock" {
-  name         = "TerraformCloudLock"
-  hash_key     = "LockID"
-  billing_mode = "PAY_PER_REQUEST"
+#resource "aws_dynamodb_table" "dynamodb_terraform_cloud_lock" {
+#  name         = "TerraformCloudLock"
+#  hash_key     = "LockID"
+#  billing_mode = "PAY_PER_REQUEST"
 
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
+#  attribute {
+#    name = "LockID"
+#    type = "S"
+#  }
 
-  tags = {
-    Name     = "Terraform state lock table for cloud common"
-  }
-}
+#  tags = {
+#    Name     = "Terraform state lock table for cloud common"
+#  }
+#}
--- parte5.tf
+++ parte5.tf
@@ -1,9 +1,9 @@
-# terraform {
-#   backend "s3" {
-#     bucket  = "practico1-parte5"
-#     key     = "terraform.tfstate"
-#     region  = "us-east-1"
-#     dynamodb_table = "terraform-lock"
-#     encrypt = true
-#   }
-# }
+terraform {
+   backend "s3" {
+     bucket  = "practico1-parte5"
+     key     = "terraform.tfstate"
+     region  = "us-east-1"
+     dynamodb_table = "TerraformCloudLock"
+     encrypt = true
+   }
+ }
